cdff3c9538d7b38289193d054ca27ad84059b6ad
Joe <jeric720@icloud.com>
2020-05-13 19:14:07 -0400

    Completed assignment 5

---
+++ src/Assignment5.java
@@ -0,0 +1,21 @@
+
+// 5.	Write a class named Assignment5 which creates an integer variable x. 
+// Set x to some value. Then write a conditional statement that checks if x is equal to 3. 
+// If so, print the message, “x is equal to 3” to the console. Write an associate else-statement 
+// that prints the message “x is NOT equal to 3” to the console.
+
+public class Assignment5 {
+	public static void main(String[] args) {
+		
+		int x = 5;
+		
+		if ( x == 3) {
+			System.out.println("x is equal to 3");
+		} else {
+			
+		} System.out.println("x is NOT equal to 3");
+	} // end if 
+
+} // end Assignment5
+
+
