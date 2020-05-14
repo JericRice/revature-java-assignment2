8aff8d5be0397655465ad2e1e1f54ab6baf729eb
Joe <jeric720@icloud.com>
2020-05-13 20:01:05 -0400

    Completed Assignment 6 - For Loop Statement

---
+++ src/Assignment7.java
@@ -0,0 +1,15 @@
+
+/*
+ * 7.	Write a class named Assignment7 which uses a for-loop to print only the even numbers between 0 and 10, inclusive. 
+ */
+
+public class Assignment7 {
+	
+	public static void main(String[] args) {
+		
+		int count = 0;
+		for (count = 0; count <= 10; count++) {
+			System.out.println(count);
+		}
+	}
+}
